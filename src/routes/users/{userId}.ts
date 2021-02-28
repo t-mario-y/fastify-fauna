04e19bb356a3bfe34e410f86c08790875f148c3e
t-mario-y <t.mario.yoshi@gmail.com>
2021-03-01 01:32:32 +0900

    feat: add GET /users/{userID} endpoint

---
+++ src/routes/users/{userId}.ts
@@ -0,0 +1,34 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import GetUserRequestParams from "@schemas/GetUserRequestParams.json";
+import { GetUserRequestParams as GetUserRequestParamsInterface } from "types/GetUserRequestParams";
+import { createFaunadbClient } from "@/faunadbClientFactory";
+import { Resource } from "fastify-autoroutes";
+import { MyFaunaError } from "@/__errors/MyFaunaHTTPError";
+import { Collection, Get, Ref } from "faunadb";
+
+export default (): Resource =>
+  <Resource>{
+    get: {
+      config: {
+        isPrivate: true,
+      },
+      schema: {
+        params: GetUserRequestParams,
+      },
+      handler: async (
+        request: FastifyRequest<{ Params: GetUserRequestParamsInterface }>,
+        reply: FastifyReply
+      ): Promise<void> => {
+        const userId = request.params.userId;
+        const client = createFaunadbClient();
+        try {
+          const result = await client.query(
+            Get(Ref(Collection("Users"), userId))
+          );
+          reply.send(result);
+        } catch (error) {
+          throw new MyFaunaError(error);
+        }
+      },
+    },
+  };
